3a091f22eb6e65f500b3bda8a3202f3c656e541d
Indi <indiboteju@Indis-MBP.lan>
2020-12-28 20:07:18 -0800

    circles

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+import Circles from './components/Circles';
+
+function App() {
+  return (
+    <div className="App">
+      <Circles/>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Circles.js
@@ -0,0 +1,45 @@
+import React, {useState} from 'react';
+import styles from './Circles.module.css'
+
+function Circles() {
+    const [color, setColor] = useState('');
+    const [boxes, setBoxes] = useState([]);
+
+    function handleSubmit(event) {
+        event.preventDefault();
+        const box = {
+            color,
+        };
+
+        setBoxes([...boxes, box])
+    }
+
+
+    return (
+        <div> 
+            <div>
+                <article>Hello!</article>
+            </div>
+
+            <form onSubmit = {handleSubmit} className='circles'>
+            <div>
+                <label><h1>Enter a Color!</h1></label>
+                <input 
+                    className={styles.circles_input}
+                    value = {color}
+                    onChange = {event => setColor(event.target.value)}
+                />
+            </div>
+            <button className={styles.button}>Submit</button>
+            </form>
+        {boxes.map((box, idx) => (
+        <div className={styles.box}  key={idx} style={{background: box.color}}></div>
+        ))}
+        </div>
+        
+        );
+}
+
+
+
+export default Circles; 
