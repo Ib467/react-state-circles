a3cbfbbee4c8b5bb1c81cc9cb0d09dea5a9bd4d4
Indi <indiboteju@Indis-MBP.lan>
2020-12-28 20:39:25 -0800

    Updates

---
+++ precache-manifest.7e6fdfe59cd4e1df3daa92f5e269635b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f261ea557021453186f784c53a586e8b",
+    "url": "/react-state-circles/index.html"
+  },
+  {
+    "revision": "3a7fb61dff32415c030f",
+    "url": "/react-state-circles/static/css/main.3d2a87c5.chunk.css"
+  },
+  {
+    "revision": "7cf343074eb9f4ed4761",
+    "url": "/react-state-circles/static/js/2.e565c775.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-state-circles/static/js/2.e565c775.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3a7fb61dff32415c030f",
+    "url": "/react-state-circles/static/js/main.25ed1913.chunk.js"
+  },
+  {
+    "revision": "c1c44646e034f0f67420",
+    "url": "/react-state-circles/static/js/runtime-main.bfb8bc74.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/react-state-circles/static/media/logo.5d5d9eef.svg"
+  }
+]);
